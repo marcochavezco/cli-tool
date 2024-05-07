7dcb8baff2cdd61e45bb93b0b71492ee9731423b
marcochavezco <machavezco@gmail.com>
2024-05-06 21:13:48 -0600

    Add chalk-animation prompt

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
 
-console.log(chalk.bgGreen('hi mom!'));
+let playerNmae;
+
+const sleep = (ms = 2000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function main() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Who Wants To Be A JavaScript Millionaire?'
+  );
+
+  await sleep();
+
+  rainbowTitle.stop();
+
+  console.log(
+    chalk.bgBlue(`  I am a process on your computer.
+  If you get any question wrong, I will be ${chalk.red('killed')}
+  So get all the questions right...`)
+  );
+}
+
+await main();
